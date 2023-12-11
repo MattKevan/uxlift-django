2c3d97ecc6bffd862aab6f75b2190df225767df4
Matt Kevan <m@kevan.tv>
2023-12-11 09:40:53 +0000

    Fix things

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,10 @@
 echo "Building project packages….."
-python3 -m pip install -r requirements.txt
+python3 pip install -r requirements.txt
 
 echo "Migrating Database..." 
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
 echo "Collecting static files..."
-python manage.py tailwind build --noinput
+python3 manage.py tailwind build --noinput
 python3 manage.py collectstatic --noinput
