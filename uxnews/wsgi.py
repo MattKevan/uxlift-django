9e94c085a317a753be970f94180ee7990ead2ce7
Matt <m@kevan.tv>
2023-12-11 00:52:34 +0000

    Update requirements

--- uxnews/wsgi.py
+++ uxnews/wsgi.py
@@ -4,4 +4,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
 
-app = application
+application = get_wsgi_application()
