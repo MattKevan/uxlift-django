0b76723206950f335adb53607e6b1a8d3f79fac6
Matt <m@kevan.tv>
2023-12-11 00:37:33 +0000

    Update requirements

--- uxnews/settings.py
+++ uxnews/settings.py
@@ -85,11 +85,11 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'default_db_name'),
-        'USER': os.getenv('DB_USER', 'default_db_user'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'default_db_password'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'NAME': os.getenv('POSTGRES_DATABASE', 'default_db_name'),
+        'USER': os.getenv('POSTGRES_USER', 'default_db_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'default_db_password'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
